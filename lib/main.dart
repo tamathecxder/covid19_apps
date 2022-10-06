89e90a299348e7846bf2425ff3dbe21433182b42
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-10-06 13:41:50 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("COVID-19 Summary"),
+        centerTitle: true,
+      ),
+      // body: ,
+    );
+  }
+}
